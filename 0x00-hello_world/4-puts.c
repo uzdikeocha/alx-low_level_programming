a7773ce5f31405eb5f421adddc97fea202c33cec
uzdikeocha <uzdikeocha@gmail.com>
2022-12-08 18:36:48 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
+/**
  *main-print a line of code using puts functions
  *
- *Return: 0
+ *Return:0
  */
+
 int main(void)
-{ 
+{
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
